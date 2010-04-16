2e9b4fb4bff8544e71391ddbfcae6b80451dd782
lrich <leoncrich@gmail.com>
2010-04-16 14:19:34 -0400

    added page load logic

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -2,4 +2,7 @@
 Partial Class _Default
     Inherits System.Web.UI.Page
 
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Response.Write("Hiya World")
+    End Sub
 End Class
