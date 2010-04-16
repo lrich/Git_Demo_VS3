ed94660f318410575b81bc51dedf7aff1412a852
lrich <leoncrich@gmail.com>
2010-04-16 14:31:21 -0400

    added new function to master

--- Default.aspx
+++ Default.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
     <div>
     Hello World
+    Hiya!
     </div>
     </form>
 </body>
--- Default.aspx.vb
+++ Default.aspx.vb
@@ -5,4 +5,8 @@ Partial Class _Default
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Response.Write("Hiya World")
     End Sub
+
+    Public Sub TestThisNow()
+        Response.Write("test")
+    End Sub
 End Class
