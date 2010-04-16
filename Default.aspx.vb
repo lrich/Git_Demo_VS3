5426748e3dd13643bed06a3206b0ed7cb8d409ea
lrich <leoncrich@gmail.com>
2010-04-16 14:25:48 -0400

    Added new process

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -5,4 +5,10 @@ Partial Class _Default
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Response.Write("Hiya World")
     End Sub
+
+    Public Sub ProcessInfo()
+        While 1
+            Response.Write("Process Info")
+        End While
+    End Sub
 End Class
