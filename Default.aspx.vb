d200b223389da01a4d1f6044a50a2e1d077632a5
lrich <leoncrich@gmail.com>
2010-05-04 15:21:20 -0400

    ski

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -3,7 +3,7 @@ Partial Class _Default
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Response.Write("Hiya World")
+        Response.Write("Hiya Worldski")
         '* ProcessInfo()
     End Sub
 
